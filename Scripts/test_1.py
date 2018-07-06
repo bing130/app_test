3194a7619a1aff7fdb7a17a47f8a53e936533f9a
bing130 <578091765@qq.com>
2018-07-06 19:21:44 +0800

    添加测试步骤

--- Scripts/test_1.py
+++ Scripts/test_1.py
@@ -3,8 +3,10 @@ import allure
 
 class Test_01:
 
+    @allure.step(title="测试步骤001")
     def test_01_1(self):
         assert 1
 
+    @allure.step(title="测试步骤002")
     def test_01_2(self):
         assert 0
