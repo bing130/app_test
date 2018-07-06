4e4a29fe453de83a304fe88e7d2f2fc1abfedf0b
bing130 <578091765@qq.com>
2018-07-06 19:28:12 +0800

    添加测试描述和严重级别

--- Scripts/test_1.py
+++ Scripts/test_1.py
@@ -3,10 +3,14 @@ import allure
 
 class Test_01:
 
+    @pytest.allure.severity(pytest.allure.severity_level.TRIVIAL)
     @allure.step(title="测试步骤001")
     def test_01_1(self):
+        allure.attach('描述', '我是测试步骤001的描述～～～')
         assert 1
 
+    @pytest.allure.severity(pytest.allure.severity_level.BLOCKER)
     @allure.step(title="测试步骤002")
     def test_01_2(self):
+        allure.attach('描述', '我是测试步骤002的描述～～～')
         assert 0
